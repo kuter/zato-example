6d3b377ff94284b74ecc428175d7ccedf2eff4d7
kuter <contact@devktr.pl>
2020-03-12 17:57:02 +0100

    :bug: add json.loads

--- services/users.py
+++ services/users.py
@@ -1,9 +1,11 @@
+import json
+
 from zato.server.service import Service
 
 
 class Login(Service):
     def handle(self):
         conn = self.outgoing.plain_http.get("Login").conn
-        resp = conn.post(self.cid, self.request.payload)
+        resp = conn.post(self.cid, json.loads(self.request.payload))
 
         self.response.payload = resp.content
